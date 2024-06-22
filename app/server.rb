cc4ef9d249578171d451627aa2e62a439644c93e
Hossam Khalil <hossam.m.khalil@gmail.com>
2024-06-22 11:22:44 +0300

    remove commented line

--- app/server.rb
+++ app/server.rb
@@ -9,7 +9,6 @@ class YourRedisServer
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     puts("Logs from your program will appear here!")
 
-    # Uncomment this block to pass the first stage
     server = TCPServer.new(@port)
     client = server.accept
     client.puts "+PONG\r\n"
